7b7afb79759515c97225a8998c3ba9a1b4701a43
Андрей Романчук <andrei.rmk@gmail.com>
2022-10-18 12:38:37 +0300

    step 4

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
+
 const getName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return console.log('Hello, ' + userName + '!');
+  const userName = readlineSync.question('May I have your name? ');
+  return console.log(`Hello, ${userName}!`);
 };
 export default getName;
